e141c518b27646dd5c84fad85242c08308e92cdc
liinjianhao <1358579976@qq.com>
2017-04-19 19:25:57 +0800

    使用Mybatis通用Mapper分页插件进行分页查询

--- src/test/java/com/fengze/spring/mapper/BaseSpringTest.java
+++ src/test/java/com/fengze/spring/mapper/BaseSpringTest.java
@@ -14,9 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 /*加载Spring容器，初始化上下文对象*/
 @ContextConfiguration(locations={"classpath*:applicationContext.xml"})
 // 添加注释@Transactional 回滚对数据库操作
-/*当设置为@Transactional(propagation= Propagation.NOT_SUPPORTED)时，执行测试的时候不会提交
-* 当设置为@Transactional；由于是由Spring托管的，Spring会自动将其提交*/
-@Transactional(propagation= Propagation.NOT_SUPPORTED)
-//@Transactional
+/*当设置为@Transactional(propagation= Propagation.NOT_SUPPORTED)时其目的是不让事务起作用；
+由于是由Spring托管的，Spring会自动将其提交
+* 当设置为@Transactional，意味着打开事务,执行测试的时候不会提交*/
+//@Transactional(propagation= Propagation.NOT_SUPPORTED)
+@Transactional
 public class BaseSpringTest {
 }
--- src/test/java/com/fengze/spring/mapper/UserDoMapperTest.java
+++ src/test/java/com/fengze/spring/mapper/UserDoMapperTest.java
@@ -1,11 +1,15 @@
 package com.fengze.spring.mapper;
 
 import com.fengze.spring.model.UserDo;
+import com.github.pagehelper.PageHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -36,4 +40,40 @@ public class UserDoMapperTest extends BaseSpringTest {
         log.info("查询的结果是：{}",res.toString());
 
     }
+    @Test
+    public void  testAddUser(){
+         /*按照主键Userid 查询User*/
+        UserDo user=new UserDo();
+
+        user.setUserName("李达康a");
+        user.setRealName("李达a");
+        user.setUserType("普通会员");
+        user.setEmail("1358599615@qq.com");
+        user.setState("1");
+        user.setAddress("河南郑州");
+        user.setCellPhone("15806624565");
+        user.setPassWord("123456");
+        user.setCreateBy("admin");
+        user.setCreateDate(new Date());
+        user.setUpdateBy("admin");
+        user.setUpdateDate(new Date());
+
+        Integer res=userDoMapper.insert(user);
+
+        log.info("查询的结果是：{}",res);
+
+    }
+    /*分页测试*/
+    @Test
+    public void testSelectAll(){
+        UserDo user=new UserDo();
+
+        PageHelper.offsetPage(1, 2);
+
+        /*List<UserDo>中的UserDo指的是所要分页的数据表对应的实体类*/
+        List<UserDo> page=userDoMapper.selectAll();
+
+        log.info("分页的结果：{}",page);
+    }
+
 }
